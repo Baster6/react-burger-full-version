15458ab721b35801db96fbede976da06fe1615f3
Ahmad <94278280+Baster6@users.noreply.github.com>
2021-12-04 16:04:10 +0500

    Add files via upload

---
+++ precache-manifest.b7094b036b1667dcb427d0f4615d469e.js
@@ -0,0 +1,38 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "2fc725c3836e57b06d02dfb335ede3d0",
+    "url": "/index.html"
+  },
+  {
+    "revision": "11c04b3eb60a04160979",
+    "url": "/static/css/main.b389e2f0.chunk.css"
+  },
+  {
+    "revision": "e06cca3c14347a06fe3b",
+    "url": "/static/js/2.c2731b83.chunk.js"
+  },
+  {
+    "revision": "11c04b3eb60a04160979",
+    "url": "/static/js/main.414f64d5.chunk.js"
+  },
+  {
+    "revision": "fa27f71bd95d5ca81881",
+    "url": "/static/js/runtime-main.cf8cab74.js"
+  },
+  {
+    "revision": "385ae8f1201379005ad5f888db39e059",
+    "url": "/static/media/burger-logo-4.385ae8f1.jpg"
+  },
+  {
+    "revision": "1d4aace8ece50372c4a71e8334f161a1",
+    "url": "/static/media/down.1d4aace8.svg"
+  },
+  {
+    "revision": "c4a0f29538534d8a0cef787104eac785",
+    "url": "/static/media/scale_1200.c4a0f295.jpg"
+  },
+  {
+    "revision": "551b5c6b9714b6eafb5739b138485841",
+    "url": "/static/media/up.551b5c6b.svg"
+  }
+]);
